436f36fbeff82c953ef4cce5c8c6c40d7985fd97
NepuShiro <nepushiro@nepunep.xyz>
2025-07-02 00:39:47 -0500

    Commit new changes

--- FluxFinder.cs
+++ FluxFinder.cs
@@ -3,93 +3,73 @@ using FrooxEngine;
 using FrooxEngine.ProtoFlux;
 using HarmonyLib;
 using ResoniteModLoader;
-using System.ComponentModel;
 
 namespace FluxFinder
 {
-	public class FluxFinder : ResoniteMod
-	{
-		public override string Name => "FluxFinder";
-		public override string Author => "NepuShiro";
-		public override string Version => "1.0.0";
-		public override string Link => "https://github.com/NepuShiro/FluxFinder/";
+    public class FluxFinder : ResoniteMod
+    {
+        public override string Name => "FluxFinder";
+        public override string Author => "NepuShiro";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/NepuShiro/FluxFinder/";
 
-		[AutoRegisterConfigKey]
-		private static ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("enabled", "Should FluxFinder be Enabled?", () => true);
-		[AutoRegisterConfigKey]
-		private static ModConfigurationKey<string> EQUIPEDDV = new ModConfigurationKey<string>("Equiped DV", "The DynamicVariable<bool> to set if the Tip is Equipped", () => "World/nepu.istipequiped");
-		[AutoRegisterConfigKey]
-		private static ModConfigurationKey<string> COLORDV = new ModConfigurationKey<string>("Color DV", "The DynamicVariable<colorX> for the Hovered Color", () => "World/nepu.allhovercolor");
+        [AutoRegisterConfigKey] private static ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("enabled", "Should FluxFinder be Enabled?", () => true);
+        [AutoRegisterConfigKey] private static ModConfigurationKey<string> EQUIPEDDV = new ModConfigurationKey<string>("Equiped DV", "The DynamicVariable<bool> to set if the Tip is Equipped", () => "World/nepu.istipequiped");
+        [AutoRegisterConfigKey] private static ModConfigurationKey<string> COLORDV = new ModConfigurationKey<string>("Color DV", "The DynamicVariable<colorX> for the Hovered Color", () => "World/nepu.allhovercolor");
 
-		private static ModConfiguration config;
-		private static string colorDV;
-		private static string equipedDV;
+        private static ModConfiguration _config;
+        private static string ColorDv => _config.GetValue(COLORDV);
+        private static string EquipedDv => _config.GetValue(EQUIPEDDV);
 
-		public override void OnEngineInit()
-		{
-			config = GetConfiguration();
-			config.Save(true);
+        public override void OnEngineInit()
+        {
+            _config = GetConfiguration();
+            _config!.Save(true);
 
-			Harmony harmony = new("net.NepuShiro.FluxFinder");
-			harmony.PatchAll();
-		}
+            Harmony harmony = new Harmony("net.NepuShiro.FluxFinder");
+            harmony.PatchAll();
+        }
 
-		[HarmonyPatch(typeof(ProtoFluxTool))]
-		private class ProtoFluxToolPatch
-		{
-			static Slot fluxFinderSlot;
+        [HarmonyPatch(typeof(ProtoFluxTool))]
+        private class ProtoFluxToolPatch
+        {
+            private static Slot _fluxFinderSlot;
+            private static colorX _oldColor;
 
-			[HarmonyPrefix, HarmonyPatch("OnEquipped")]
-			static void OnEquippedPatch(ProtoFluxTool __instance)
-			{
-				if (!config.GetValue(ENABLED)) return;
+            [HarmonyPrefix, HarmonyPatch("OnEquipped")]
+            private static void OnEquippedPatch(ProtoFluxTool __instance)
+            {
+                if (!_config.GetValue(ENABLED) || !__instance.Slot.ActiveUser.IsLocalUser) return;
 
-				var userSlot = __instance.LocalUser.Root.Slot;
-				var funnySlot = userSlot.FindChildOrAdd("Injected - FluxFinder", false);
-				
-				colorDV = config.GetValue(COLORDV);
-				equipedDV = config.GetValue(EQUIPEDDV);
-				
-				if (funnySlot != null)
-				{
-					fluxFinderSlot = funnySlot;
+                _fluxFinderSlot = __instance.LocalUser.Root.Slot.FindChildOrAdd("Injected - FluxFinder", false);
+                if (_fluxFinderSlot == null) return;
 
-					var funnyvar = funnySlot.GetComponentsInChildren<DynamicValueVariable<colorX>>();
-					if (funnyvar.Count == 0)
-					{
-						var fluxvar1 = funnySlot.AttachComponent<DynamicValueVariable<colorX>>();
-						fluxvar1.Persistent = false;
-						fluxvar1.VariableName.Value = colorDV;
-						var equippedVar2 = funnySlot.AttachComponent<DynamicValueVariable<bool>>();
-						equippedVar2.Persistent = false;
-						equippedVar2.VariableName.Value = equipedDV;
-						equippedVar2.Value.Value = true;
-					}
+                DynamicValueVariable<colorX> fluxVar1 = _fluxFinderSlot.GetComponentOrAttach<DynamicValueVariable<colorX>>();
+                fluxVar1.Persistent = false;
+                fluxVar1.VariableName.Value = ColorDv;
+                fluxVar1.Value.Value = __instance.HoveringElementColor.Value;
+                DynamicValueVariable<bool> equippedVar2 = _fluxFinderSlot.GetComponentOrAttach<DynamicValueVariable<bool>>();
+                equippedVar2.Persistent = false;
+                equippedVar2.VariableName.Value = EquipedDv;
+                equippedVar2.Value.Value = true;
+            }
 
-					funnySlot.WriteDynamicVariable(equipedDV, true);
-					return;
-				};
-			}
+            [HarmonyPrefix, HarmonyPatch("OnDequipped")]
+            private static void OnDequippedPatch(ProtoFluxTool __instance)
+            {
+                if (!_config.GetValue(ENABLED) || !__instance.Slot.ActiveUser.IsLocalUser) return;
 
-			[HarmonyPrefix, HarmonyPatch("OnDequipped")]
-			static void OnDequippedPatch(ProtoFluxTool __instance)
-			{
-				if (fluxFinderSlot == null || !config.GetValue(ENABLED)) return;
+                _fluxFinderSlot?.WriteDynamicVariable(ColorDv, colorX.MinValue);
+                _fluxFinderSlot?.WriteDynamicVariable(EquipedDv, false);
+            }
 
-				colorDV = config.GetValue(COLORDV);
-				equipedDV = config.GetValue(EQUIPEDDV);
-				
-				fluxFinderSlot.WriteDynamicVariable(equipedDV, false);
-				fluxFinderSlot.WriteDynamicVariable(colorDV, new colorX(0f, 0f, 0f, 0f));
-			}
+            [HarmonyPrefix, HarmonyPatch("Update")]
+            private static void UpdatePatch(ProtoFluxTool __instance)
+            {
+                if (!_config.GetValue(ENABLED) || !__instance.Slot.ActiveUser.IsLocalUser) return;
 
-			[HarmonyPrefix, HarmonyPatch("Update")]
-			static void UpdatePatch(ProtoFluxTool __instance)
-			{
-				if (fluxFinderSlot == null || !config.GetValue(ENABLED)) return;
-
-				fluxFinderSlot.WriteDynamicVariable(colorDV, __instance.HoveringElementColor.Value);
-			}
-		}
-	}
+                _fluxFinderSlot?.WriteDynamicVariable(ColorDv, __instance.HoveringElementColor.Value);
+            }
+        }
+    }
 }
